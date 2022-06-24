d4fd3776b711d0925e6a67ef9447220561412d77
Matt Evans <81327996+nhpip@users.noreply.github.com>
2022-06-24 16:08:45 -0400

    Cleaned up help for ex_doc's benefit

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Variadic.MixProject do
 
   def project do
     [
-      app: :variadic,
+      app: :variadic_elixir,
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
